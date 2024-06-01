df319bfd2eeb84cdc2f185de5b820d5642002359
boobaGreen <devboobagreen@gmail.com>
2024-06-02 01:10:26 +0200

    co2 added calculation 4

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require("dotenv").config(); // Carica le variabili di ambiente da .env
-var co2 = require("@tgwf/co2").co2;
 var express = require("express");
 var Telegraf = require("telegraf").Telegraf;
 var app = express();
@@ -7,66 +6,57 @@ var app = express();
 var bot = new Telegraf("7317510692:AAF20M_I-Gz8g8PCnbE3fPjCnwRM9cKF784");
 var totalMessages = 0;
 var totalSizeKB = 0;
-var oneByte = new co2({ model: "1byte" });
 var calculateMessageSizeKB = function (message) {
-  if (message.text) {
-    var messageSizeBytes = Buffer.byteLength(message.text, "utf8");
-    return (messageSizeBytes / 1024).toFixed(3);
-  } else if (message.photo) {
-    var photoSizeBytes = message.photo[message.photo.length - 1].file_size;
-    return (photoSizeBytes / 1024).toFixed(3);
-  } else if (message.voice) {
-    var voiceSizeBytes = message.voice.file_size;
-    return (voiceSizeBytes / 1024).toFixed(3);
-  } else if (message.video) {
-    var videoSizeBytes = message.video.file_size;
-    return (videoSizeBytes / 1024).toFixed(3);
-  } else if (message.document) {
-    var documentSizeBytes = message.document.file_size;
-    return (documentSizeBytes / 1024).toFixed(3);
-  } else {
-    return "Tipo di messaggio non supportato";
-  }
+    if (message.text) {
+        var messageSizeBytes = Buffer.byteLength(message.text, "utf8");
+        return (messageSizeBytes / 1024).toFixed(3);
+    }
+    else if (message.photo) {
+        var photoSizeBytes = message.photo[message.photo.length - 1].file_size;
+        return (photoSizeBytes / 1024).toFixed(3);
+    }
+    else if (message.voice) {
+        var voiceSizeBytes = message.voice.file_size;
+        return (voiceSizeBytes / 1024).toFixed(3);
+    }
+    else if (message.video) {
+        var videoSizeBytes = message.video.file_size;
+        return (videoSizeBytes / 1024).toFixed(3);
+    }
+    else if (message.document) {
+        var documentSizeBytes = message.document.file_size;
+        return (documentSizeBytes / 1024).toFixed(3);
+    }
+    else {
+        return "Tipo di messaggio non supportato";
+    }
 };
+var co2 = require("@tgwf/co2").co2;
+var oneByte = new co2({ model: "1byte" });
 console.log("Bot started");
 bot.start(function (ctx) {
-  return ctx.reply(
-    "This message show when you use the /start command on the bot"
-  );
+    return ctx.reply("This message show when you use the /start command on the bot");
 });
 bot.help(function (ctx) {
-  return ctx.reply(
-    "This message show when you use the /help command on the bot"
-  );
+    return ctx.reply("This message show when you use the /help command on the bot");
 });
 bot.on("message", function (ctx) {
-  totalMessages++;
-  var messageSizeKB = parseFloat(calculateMessageSizeKB(ctx.message)); // Calcola la dimensione del messaggio in KB
-  totalSizeKB += messageSizeKB;
-  console.log("Contenuto del messaggio:", ctx.message);
-  var totalSizeBytes = totalSizeKB * 1024;
-  var emissions = oneByte.perByte(totalSizeBytes);
-  ctx.reply(
-    "La dimensione del messaggio \u00E8 di "
-      .concat(messageSizeKB, " KB. Totale messaggi: ")
-      .concat(totalMessages, ". Peso totale: ")
-      .concat(
-        totalSizeKB.toFixed(3),
-        " e le emissioni di CO2 associate sono di "
-      )
-      .concat(emissions, " kg.")
-  );
-  ctx.reply(
-    "Contenuto totale di ctx.message:".concat(JSON.stringify(ctx.message))
-  );
+    totalMessages++;
+    var messageSizeKB = parseFloat(calculateMessageSizeKB(ctx.message)); // Calcola la dimensione del messaggio in KB
+    totalSizeKB += messageSizeKB;
+    console.log("Contenuto del messaggio:", ctx.message);
+    var totalSizeBytes = totalSizeKB * 1024;
+    var emissions = oneByte.perByte(totalSizeBytes);
+    ctx.reply("La dimensione del messaggio \u00E8 di ".concat(messageSizeKB, " KB. Totale messaggi: ").concat(totalMessages, ". Peso totale: ").concat(totalSizeKB.toFixed(3), " e le emissioni di CO2 associate sono di ").concat(emissions, " kg."));
+    ctx.reply("Contenuto totale di ctx.message:".concat(JSON.stringify(ctx.message)));
 });
 bot.launch();
 // Imposta una semplice rotta per verificare che il server sia in esecuzione
 app.get("/", function (req, res) {
-  res.send("Server is running and bot is active.");
+    res.send("Server is running and bot is active.");
 });
 // Avvia il server Express
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
-  console.log("Server is running on port ".concat(PORT));
+    console.log("Server is running on port ".concat(PORT));
 });
