4152e1b48ae2f5d45cd363091dcc7275a8878e05
boobaGreen <devboobagreen@gmail.com>
2024-06-07 21:23:49 +0200

    15 min

--- src/index.ts
+++ src/index.ts
@@ -92,7 +92,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
 
-  cron.schedule("* * * * *", () => {
+  cron.schedule("*/15 * * * *", () => {
     console.log("Esecuzione del job di invio report ogni minuto !");
     if (Object.keys(groupStats).length > 0) {
       sendReport();
