e90f6c674efb0c8e3dd61086e468924fe7d6510e
boobaGreen <devboobagreen@gmail.com>
2024-07-16 11:47:00 +0200

    rf report send

--- build/utils/reportUtils.js
+++ build/utils/reportUtils.js
@@ -41,8 +41,6 @@ const sendReportData = (payload) => __awaiter(void 0, void 0, void 0, function* 
     }
 });
 const createReportPayload = (chatId, stats, groupName = "", participantsCount = 0, adminIds = []) => {
-    console.log("dentro *****************************++ createReportPayload");
-    console.log("stats in createReportPayload prima", stats);
     const totalSizeBytes = stats.totalSizeKB * 1024;
     const textTotalSizeBytes = stats.textTotalSize * 1024;
     const photoTotalSizeBytes = stats.photoTotalSize * 1024;
@@ -50,69 +48,142 @@ const createReportPayload = (chatId, stats, groupName = "", participantsCount = 
     const documentTotalSizeBytes = stats.documentTotalSize * 1024;
     const voiceTotalSizeBytes = stats.voiceTotalSize * 1024;
     const stickerTotalSizeBytes = stats.stickerTotalSize * 1024;
-    console.log("stats in createReportPayload seconda", stats);
-    const emissionsOneByte = oneByte
-        .update(totalSizeBytes)
-        .text(textTotalSizeBytes)
-        .photo(photoTotalSizeBytes)
-        .video(videoTotalSizeBytes)
-        .voice(voiceTotalSizeBytes)
-        .document(documentTotalSizeBytes)
-        .sticker(stickerTotalSizeBytes);
-    console.log("stats in createReportPayload terza", stats);
-    const emissionsSWD = swd
-        .update(totalSizeBytes)
-        .text(textTotalSizeBytes)
-        .photo(photoTotalSizeBytes)
-        .video(videoTotalSizeBytes)
-        .voice(voiceTotalSizeBytes)
-        .document(documentTotalSizeBytes)
-        .sticker(stickerTotalSizeBytes);
+    const pollTotalSizeBytes = stats.pollTotalSize * 1024;
+    const emissionsOneByteMethod = oneByte.perByte(totalSizeBytes).toFixed(7);
+    const emissionsSWDMethod = swd.perByte(totalSizeBytes).toFixed(7);
+    const textEmissionsOneByteMethod = oneByte
+        .perByte(textTotalSizeBytes)
+        .toFixed(7);
+    const textEmissionsSWDMethod = swd.perByte(textTotalSizeBytes).toFixed(7);
+    const photoEmissionsOneByteMethod = oneByte
+        .perByte(photoTotalSizeBytes)
+        .toFixed(7);
+    const photoEmissionsSWDMethod = swd.perByte(photoTotalSizeBytes).toFixed(7);
+    const videoEmissionsOneByteMethod = oneByte
+        .perByte(videoTotalSizeBytes)
+        .toFixed(7);
+    const videoEmissionsSWDMethod = swd.perByte(videoTotalSizeBytes).toFixed(7);
+    const documentEmissionsOneByteMethod = oneByte
+        .perByte(documentTotalSizeBytes)
+        .toFixed(7);
+    const documentEmissionsSWDMethod = swd
+        .perByte(documentTotalSizeBytes)
+        .toFixed(7);
+    const voiceEmissionsOneByteMethod = oneByte
+        .perByte(voiceTotalSizeBytes)
+        .toFixed(7);
+    const voiceEmissionsSWDMethod = swd.perByte(voiceTotalSizeBytes).toFixed(7);
+    const stickerEmissionsOneByteMethod = oneByte
+        .perByte(stickerTotalSizeBytes)
+        .toFixed(7);
+    const stickerEmissionsSWDMethod = swd
+        .perByte(stickerTotalSizeBytes)
+        .toFixed(7);
+    const pollEmissionsOneByteMethod = oneByte
+        .perByte(pollTotalSizeBytes)
+        .toFixed(7);
+    const pollEmissionsSWDMethod = swd.perByte(pollTotalSizeBytes).toFixed(7);
+    let totalMessages = stats.totalMessages || 0;
+    let totalSizeKB = stats.totalSizeKB || 0;
+    let emissionsOneByte = isNaN(parseFloat(emissionsOneByteMethod))
+        ? 0
+        : parseFloat(emissionsOneByteMethod);
+    let emissionsSWD = isNaN(parseFloat(emissionsSWDMethod))
+        ? 0
+        : parseFloat(emissionsSWDMethod);
+    let textTotalMessages = stats.textTotalMessages || 0;
+    let textTotalSize = stats.textTotalSize || 0;
+    let textEmissionsOneByte = isNaN(parseFloat(textEmissionsOneByteMethod))
+        ? 0
+        : parseFloat(textEmissionsOneByteMethod);
+    let textEmissionsSWD = isNaN(parseFloat(textEmissionsSWDMethod))
+        ? 0
+        : parseFloat(textEmissionsSWDMethod);
+    let photoTotalMessages = stats.photoTotalMessages || 0;
+    let photoTotalSize = stats.photoTotalSize || 0;
+    let photoEmissionsOneByte = isNaN(parseFloat(photoEmissionsOneByteMethod))
+        ? 0
+        : parseFloat(photoEmissionsOneByteMethod);
+    let photoEmissionsSWD = isNaN(parseFloat(photoEmissionsSWDMethod))
+        ? 0
+        : parseFloat(photoEmissionsSWDMethod);
+    let videoTotalMessages = stats.videoTotalMessages || 0;
+    let videoTotalSize = stats.videoTotalSize || 0;
+    let videoEmissionsOneByte = isNaN(parseFloat(videoEmissionsOneByteMethod))
+        ? 0
+        : parseFloat(videoEmissionsOneByteMethod);
+    let videoEmissionsSWD = isNaN(parseFloat(videoEmissionsSWDMethod))
+        ? 0
+        : parseFloat(videoEmissionsSWDMethod);
+    let documentTotalMessages = stats.documentTotalMessages || 0;
+    let documentTotalSize = stats.documentTotalSize || 0;
+    let documentEmissionsOneByte = isNaN(parseFloat(documentEmissionsOneByteMethod))
+        ? 0
+        : parseFloat(documentEmissionsOneByteMethod);
+    let documentEmissionsSWD = isNaN(parseFloat(documentEmissionsSWDMethod))
+        ? 0
+        : parseFloat(documentEmissionsSWDMethod);
+    let pollTotalMessages = stats.pollTotalMessages || 0;
+    let pollTotalSize = stats.pollTotalSize || 0;
+    let pollEmissionsOneByte = isNaN(parseFloat(pollEmissionsOneByteMethod))
+        ? 0
+        : parseFloat(pollEmissionsOneByteMethod);
+    let pollEmissionsSWD = isNaN(parseFloat(pollEmissionsSWDMethod))
+        ? 0
+        : parseFloat(pollEmissionsSWDMethod);
+    let stickerTotalMessages = stats.stickerTotalMessages || 0;
+    let stickerTotalSize = stats.stickerTotalSize || 0;
+    let stickerEmissionsOneByte = isNaN(parseFloat(stickerEmissionsOneByteMethod))
+        ? 0
+        : parseFloat(stickerEmissionsOneByteMethod);
+    let stickerEmissionsSWD = isNaN(parseFloat(stickerEmissionsSWDMethod))
+        ? 0
+        : parseFloat(stickerEmissionsSWDMethod);
+    let voiceTotalMessages = stats.voiceTotalMessages || 0;
+    let voiceTotalSize = stats.voiceTotalSize || 0;
+    let voiceEmissionsOneByte = isNaN(parseFloat(voiceEmissionsOneByteMethod))
+        ? 0
+        : parseFloat(voiceEmissionsOneByteMethod);
+    let voiceEmissionsSWD = isNaN(parseFloat(voiceEmissionsSWDMethod))
+        ? 0
+        : parseFloat(voiceEmissionsSWDMethod);
     return {
         groupId: chatId,
-        totalMessages: stats.totalMessages,
-        totalSizeKB: stats.totalSizeKB,
-        emissionsOneByteMethod: emissionsOneByte.toFixed(7),
-        emissionsSWDMethod: emissionsSWD.toFixed(7),
-        textTotalMessages: stats.textTotalMessages,
-        textTotalSize: stats.textTotalSize,
-        textEmissionsOneByteMethod: oneByte.text(textTotalSizeBytes).toFixed(7),
-        textEmissionsSWDMethod: swd.text(textTotalSizeBytes).toFixed(7),
-        photoTotalMessages: stats.photoTotalMessages,
-        photoTotalSize: stats.photoTotalSize,
-        photoEmissionsOneByteMethod: oneByte.photo(photoTotalSizeBytes).toFixed(7),
-        photoEmissionsSWDMethod: swd.photo(photoTotalSizeBytes).toFixed(7),
-        videoTotalMessages: stats.videoTotalMessages,
-        videoTotalSize: stats.videoTotalSize,
-        videoEmissionsOneByteMethod: oneByte.video(videoTotalSizeBytes).toFixed(7),
-        videoEmissionsSWDMethod: swd.video(videoTotalSizeBytes).toFixed(7),
-        voiceTotalMessages: stats.voiceTotalMessages,
-        voiceTotalSize: stats.voiceTotalSize,
-        voiceEmissionsOneByteMethod: oneByte.voice(voiceTotalSizeBytes).toFixed(7),
-        voiceEmissionsSWDMethod: swd.voice(voiceTotalSizeBytes).toFixed(7),
-        documentTotalMessages: stats.documentTotalMessages,
-        documentTotalSize: stats.documentTotalSize,
-        documentEmissionsOneByteMethod: oneByte
-            .document(documentTotalSizeBytes)
-            .toFixed(7),
-        documentEmissionsSWDMethod: swd.document(documentTotalSizeBytes).toFixed(7),
-        pollTotalMessages: stats.pollTotalMessages,
-        pollTotalSize: stats.pollTotalSize,
-        pollEmissionsOneByteMethod: oneByte
-            .poll(stats.pollTotalSize * 1024)
-            .toFixed(7),
-        pollEmissionsSWDMethod: swd.poll(stats.pollTotalSize * 1024).toFixed(7),
-        stickerTotalMessages: stats.stickerTotalMessages,
-        stickerTotalSize: stats.stickerTotalSize,
-        stickerEmissionsOneByteMethod: oneByte
-            .sticker(stats.stickerTotalSize * 1024)
-            .toFixed(7),
-        stickerEmissionsSWDMethod: swd
-            .sticker(stats.stickerTotalSize * 1024)
-            .toFixed(7),
-        groupName: groupName || "",
-        participantsCount: participantsCount || 0,
-        adminIds: adminIds,
+        totalMessages: totalMessages,
+        totalSizeKB: totalSizeKB,
+        emissionsOneByteMethod: emissionsOneByte,
+        emissionsSWDMethod: emissionsSWD,
+        textTotalMessages,
+        textTotalSize,
+        textEmissionsOneByteMethod: textEmissionsOneByte,
+        textEmissionsSWDMethod: textEmissionsSWD,
+        photoTotalMessages,
+        photoTotalSize,
+        photoEmissionsOneByteMethod: photoEmissionsOneByte,
+        photoEmissionsSWDMethod: photoEmissionsSWD,
+        videoTotalMessages,
+        videoTotalSize,
+        videoEmissionsOneByteMethod: videoEmissionsOneByte,
+        videoEmissionsSWDMethod: videoEmissionsSWD,
+        voiceTotalMessages: voiceTotalMessages,
+        voiceTotalSize: voiceTotalSize,
+        voiceEmissionsOneByteMethod: voiceEmissionsOneByte,
+        voiceEmissionsSWDMethod: voiceEmissionsSWD,
+        documentTotalMessages,
+        documentTotalSize,
+        documentEmissionsOneByteMethod: documentEmissionsOneByte,
+        documentEmissionsSWDMethod: documentEmissionsSWD,
+        pollTotalMessages,
+        pollTotalSize,
+        pollEmissionsOneByteMethod: pollEmissionsOneByte,
+        pollEmissionsSWDMethod: pollEmissionsSWD,
+        stickerTotalMessages,
+        stickerTotalSize,
+        stickerEmissionsOneByteMethod: stickerEmissionsOneByte,
+        stickerEmissionsSWDMethod: stickerEmissionsSWD,
+        groupName: groupName,
+        participantsCount, // Aggiungi il numero di partecipanti al payload
+        adminIds, // Aggiungi i nomi degli amministratori al payload
     };
 };
 const sendEmptyReport = (chatId, chatInfo) => __awaiter(void 0, void 0, void 0, function* () {
@@ -150,25 +221,25 @@ const sendReport = (groupStats, chatInfos) => __awaiter(void 0, void 0, void 0, 
         const payload = createReportPayload(chatId, stats, chatInfo.title || "", chatInfo.membersCount || 0, chatInfo.adminIds || []);
         console.log("payload poco prima di spedire *******", payload);
         yield sendReportData(payload);
+        // Azzeriamo solo i contatori dopo l'invio del report
+        groupStats[chatId] = {
+            totalMessages: 0,
+            totalSizeKB: 0,
+            textTotalMessages: 0,
+            textTotalSize: 0,
+            photoTotalMessages: 0,
+            photoTotalSize: 0,
+            videoTotalMessages: 0,
+            videoTotalSize: 0,
+            voiceTotalMessages: 0,
+            voiceTotalSize: 0,
+            documentTotalMessages: 0,
+            documentTotalSize: 0,
+            pollTotalMessages: 0,
+            pollTotalSize: 0,
+            stickerTotalMessages: 0,
+            stickerTotalSize: 0,
+        };
     }
 });
 exports.sendReport = sendReport;
-// // Azzeriamo solo i contatori dopo l'invio del report
-// groupStats[chatId] = {
-//   totalMessages: 0,
-//   totalSizeKB: 0,
-//   textTotalMessages: 0,
-//   textTotalSize: 0,
-//   photoTotalMessages: 0,
-//   photoTotalSize: 0,
-//   videoTotalMessages: 0,
-//   videoTotalSize: 0,
-//   voiceTotalMessages: 0,
-//   voiceTotalSize: 0,
-//   documentTotalMessages: 0,
-//   documentTotalSize: 0,
-//   pollTotalMessages: 0,
-//   pollTotalSize: 0,
-//   stickerTotalMessages: 0,
-//   stickerTotalSize: 0,
-// };
